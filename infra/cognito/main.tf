2569c3bcb637eeea549dab87d3d2afd890c995c9
lf-ishwarkhadka <ishwarkhadka@lftechnology.com>
2024-08-07 17:37:45 +0545

    cognito updated

--- infra/cognito/main.tf
+++ infra/cognito/main.tf
@@ -1,4 +1,12 @@
 resource "aws_cognito_user_pool" "default" {
- name =var.cognito_name 
- tags = var.tags
+  name = var.cognito_name
+
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length = 6
+  }
+
+  tags = var.tags
 }
