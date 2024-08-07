9c31487ef78eb2e9bfe8b1020d801f154f3c6503
lf-ishwarkhadka <ishwarkhadka@lftechnology.com>
2024-08-07 17:49:32 +0545

    cognito user verification email in terraform

--- infra/cognito/main.tf
+++ infra/cognito/main.tf
@@ -8,12 +8,21 @@ resource "aws_cognito_user_pool" "default" {
     minimum_length = 6
   }
 
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject        = "Account Confirmation"
+    email_message        = "Your confirmation code is {####}"
+  }
+
   account_recovery_setting {
     recovery_mechanism {
       name     = "verified_email"
       priority = 1
     }
   }
+
+
 
   tags = var.tags
 }
