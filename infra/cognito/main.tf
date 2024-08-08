b755534272ff9563c0efbbf7ff0af8206e45c7ae
lf-ishwarkhadka <ishwarkhadka@lftechnology.com>
2024-08-08 13:54:08 +0545

    update cognito user pool

--- infra/cognito/main.tf
+++ infra/cognito/main.tf
@@ -8,6 +8,14 @@ resource "aws_cognito_user_pool" "default" {
     minimum_length = 6
   }
 
+  schema {
+    name                     = "foo"
+    attribute_data_type      = "String"
+    mutable                  = false
+    required                 = true
+    developer_only_attribute = false
+    string_attribute_constraints {}
+  }
 
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
@@ -22,7 +30,23 @@ resource "aws_cognito_user_pool" "default" {
     }
   }
 
-
-
   tags = var.tags
+}
+
+
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = "cognito-client"
+
+  user_pool_id = aws_cognito_user_pool.default.id
+  generate_secret = false
+  refresh_token_validity = 90
+  prevent_user_existence_errors = "ENABLED"
+  callback_urls = ["https://d1o23vstj4q9h8.cloudfront.net"]
+  
+}
+
+resource "aws_cognito_user_pool_domain" "cognito-domain" {
+  domain       = "ishwarkhadka"
+  user_pool_id = "${aws_cognito_user_pool.default.id}"
 }
