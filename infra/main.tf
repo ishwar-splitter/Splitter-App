9499114fccd95dbea874fc60bafaca885eacb0e6
lf-ishwarkhadka <ishwarkhadka@lftechnology.com>
2024-09-01 20:15:33 +0545

    Create complete VPC

--- infra/main.tf
+++ infra/main.tf
@@ -13,17 +13,21 @@ module "cognito" {
   tags         = local.default_tags
 }
 
-# module "vpc" {
-#   source = "./VPC"
+module "vpc" {
+  source = "./vpc"
 
-#   vpc_cidr              = "10.0.0.0/16"
-#   vpc_name              = "splitter-vpc"
-#   public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
-#   private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
-#   public_subnet_count    = 2
-#   private_subnet_count   = 2
-#   availability_zones     = ["us-east-1a", "us-east-1b"]
-# }
+  vpc_name = "ishwar-splitter-vpc"
+  cidr = "10.0.0.0/16"
+  azs = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  database_subnets = ["10.0.151.0/24", "10.0.152.0/24"]
+  create_database_subnet_group = true
+  create_database_subnet_route_table = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags = local.default_tags
+}
 
 resource "aws_ecr_repository" "splitter_api" {
   name                 = "splitter-api"
